4ecb68bef8f9506406925ea647dac45c994ac537
RWaelen <32195208+RWaelen@users.noreply.github.com>
2017-11-01 14:35:49 +0100

    Listitems clickevent gaat naar PlayerPage

--- Touchyie/Touchyie/SearchItem.cs
+++ Touchyie/Touchyie/SearchItem.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace Touchyie
 {
+    
     public class SearchItem
     {
         public string VidThumbnail { get; set; }
         public string FilmName { get; set; }
         public string VideoDetail { get; set; }
         public string TijdsDuur { get; set; }
+
+        
     }
 }
