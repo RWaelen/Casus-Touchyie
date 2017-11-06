860e7e438977b663f04acaf3f6d95014abcefdf7
juan <33056634+martinezjuan@users.noreply.github.com>
2017-11-06 13:49:33 +0100

    comments aan alle c# + xaml gegeven

--- Touchyie/Touchyie/InlogPage.xaml.cs
+++ Touchyie/Touchyie/InlogPage.xaml.cs
@@ -16,28 +16,32 @@ namespace Touchyie
 		{
 			InitializeComponent ();
 
+            //hier maak je mogelijk om de label wachtwoord vergeten te klikken
             var tapwachtwoord = new TapGestureRecognizer();
             tapwachtwoord.Tapped += Tapwachtwoord_Tapped;
             wachtwoord.GestureRecognizers.Add(tapwachtwoord);
 
+            //Hier maak je mogelijk om de label registreren te klikken
             var tapregistreren = new TapGestureRecognizer();
             tapregistreren.Tapped += Tapregistreren_Tapped;
             registreren.GestureRecognizers.Add(tapregistreren);
 
+            //hier maak je mogelijk om een event gebeuren als je klikt op de loginbutton
             Button loginbuttontest = Content.FindByName<Button>("loginButton");
        loginbuttontest.Clicked += Loginbutton_Clicked;
 		}
 
+        //Hier kies je wat de event zal zijn als je de registreren label klikt
         private async void Tapregistreren_Tapped(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new RegistratiePage());
         }
-
+        //Hier kies je wat de event zal zijn als je de 'wachtwoord vergeten' label klikt
         private async void Tapwachtwoord_Tapped(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new MainPage());
         }
-
+        //Hier kies je wat de event zal zijn als je de login button klikt
         private async void Loginbutton_Clicked(object sender, EventArgs e)
         {
           await Navigation.PushModalAsync(new MainPage());
