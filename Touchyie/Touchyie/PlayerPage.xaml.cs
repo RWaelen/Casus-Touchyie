a6b5dadef5e845229cc723bd7e2a990703b4f1ea
RWaelen <32195208+RWaelen@users.noreply.github.com>
2017-11-06 19:59:45 +0100

    "merge" player page blader page en searchresult page

--- Touchyie/Touchyie/PlayerPage.xaml.cs
+++ Touchyie/Touchyie/PlayerPage.xaml.cs
@@ -9,12 +9,64 @@ using Xamarin.Forms.Xaml;
 
 namespace Touchyie
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PlayerPage : ContentPage
-	{
-		public PlayerPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerPage : ContentPage
+    {
+        //Twee fields gedefinieerd
+        int count = 0;
+        int counter = 0;
+        public PlayerPage()
+        {
+            InitializeComponent();
+            //Recognizers aangemaakt voor images voor click functie
+            var tapBackbutton = new TapGestureRecognizer();
+            tapBackbutton.Tapped += TapBackbutton_Tapped;
+            BackButton.GestureRecognizers.Add(tapBackbutton);
+
+            var tapPlaybutton = new TapGestureRecognizer();
+            tapPlaybutton.Tapped += TapPlaybutton_Tapped;
+            playButton.GestureRecognizers.Add(tapPlaybutton);
+
+            var tapFavoriet = new TapGestureRecognizer();
+            tapFavoriet.Tapped += TapFavoriet_Tapped;
+            favorite.GestureRecognizers.Add(tapFavoriet);
+
+        }
+        //if statement toegevoegd die zorgt voor het switchen van de ster zodat je iets kunt toevoegen aan favorieten
+        //gebruikt field counter
+        private void TapFavoriet_Tapped(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                favorite.Source = "sterVol.png";
+                counter = 1;
+            }
+            else
+            {
+                favorite.Source = "sterLeeg.png";
+                counter = 0;
+            }
+        }
+        //if statement toegevoegd die zorgt voor het switchen van de playbutton zodat je de film kunt pauzeren
+        //gebruikt field count
+        private void TapPlaybutton_Tapped(object sender, EventArgs e)
+        {
+
+            if (count == 0)
+            {
+                playButton.Source = "pauze.png";
+                count = 1;
+            }
+            else
+            {
+                playButton.Source = "play.png";
+                count = 0;
+            }
+        }
+        //wanneer er op de backbutton geklikt wordt stuurt de applicatie je terug naar de vorige pagina
+        private async void TapBackbutton_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
 }
---
+++ Touchyie/Touchyie/SearchResultPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Touchyie
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchResultPage : ContentPage
+	{
+        public SearchResultPage()
+        {
+            InitializeComponent();
+
+            ListView SRListView = Content.FindByName<ListView>("SearchResults_ListView");
+            SRListView.ItemTapped += SRListView_ItemTapped;
+
+            SearchResults_ListView.ItemsSource = new List<SearchItem>
+            {
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="6:23"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 2",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="3:28"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 3",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="8:45"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 4",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="12:25"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 5",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="3:33"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 6",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="14:20"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 7",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="7:30"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 8",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="4:02"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 6",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="14:20"
+                },
+                new SearchItem
+                {
+                    VidThumbnail="playvideo.png",
+                    FilmName="Film 7",
+                    VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="7:30"
+                },
+                new SearchItem
+                {
+                   VidThumbnail="playvideo.png",
+                    FilmName="Film 8",
+                   VideoDetail="Deze film gaat over..........",
+                    TijdsDuur="4:02"
+                },
+            };
+
+
+
+
+        }
+
+        private async void SRListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await Navigation.PushModalAsync(new PlayerPage());
+        }
+    }
+}
