e836ea518a1ac4e8779a0f35f44f853f9ed314a6
RWaelen <32195208+RWaelen@users.noreply.github.com>
2017-11-06 20:16:43 +0100

    add listview item class

---
+++ Touchyie/Touchyie/SearchItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Touchyie
+{
+    
+    public class SearchItem
+    {
+        public string VidThumbnail { get; set; }
+        public string FilmName { get; set; }
+        public string VideoDetail { get; set; }
+        public string TijdsDuur { get; set; }
+
+        
+    }
+}
